0506cb24446c086881df6923f00deb680dea9782
browolf <brother.wolf@gmail.com>
2026-02-27 17:23:11 +0000

    Add script to find PI/USDT markets on ccxt
    This script finds PI/USDT markets on ccxt and displays the current price.

---
+++ ccxt_markets_pi.py
@@ -0,0 +1,32 @@
+'''
+This script will find PI/USDT markets on ccxt and display the current price
+'''
+
+
+import ccxt
+import urllib3
+
+# Suppress SSL warnings (only needed if using verify=False)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+print("ccxt Results:")
+for exchange_id in ccxt.exchanges:
+    try:
+        exchange_class = getattr(ccxt, exchange_id)
+        exchange = exchange_class({
+            "enableRateLimit": True,
+            "verify": False,  # remove if SSL fixed properly
+        })
+
+        exchange.load_markets()
+
+        for symbol in exchange.symbols:
+            if symbol == "PI/USDT":
+                ticker = exchange.fetch_ticker(symbol)
+                last_price = ticker.get("last")
+
+               
+                print(f"Found on {exchange_id}: {symbol} @ {last_price}")
+
+    except Exception:
+        continue
